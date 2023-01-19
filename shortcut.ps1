36a2469a06851fef8676571b79bdbbaee2f43c94
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-01-19 17:22:33 +0300

    Added Microsoft Teams shortcut

--- shortcut.ps1
+++ shortcut.ps1
@@ -7,3 +7,8 @@ $ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\Outlook.lnk")
 $ShortCut.TargetPath = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
 $ShortCut.FullName 
 $ShortCut.Save()
+$ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\Microsoft Teams.lnk")
+$ShortCut.TargetPath = "$env:UserProfile\AppData\Local\Microsoft\Teams\Update.exe"
+$ShortCut.Arguments = "--processStart `"Teams.exe`""
+$ShortCut.FullName 
+$ShortCut.Save()
