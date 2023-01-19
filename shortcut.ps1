4ae202ab5719c8a44fb66b4ec51b80811469158d
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-01-19 14:41:18 +0300

    Outlook and Quick Assist shortcut

--- shortcut.ps1
+++ shortcut.ps1
@@ -3,3 +3,7 @@ $ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\quickassist.lnk")
 $ShortCut.TargetPath = "%windir%\system32\quickassist.exe"
 $ShortCut.FullName 
 $ShortCut.Save()
+$ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\Outlook.lnk")
+$ShortCut.TargetPath = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+$ShortCut.FullName 
+$ShortCut.Save()
