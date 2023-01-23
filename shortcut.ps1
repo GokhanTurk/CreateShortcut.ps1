c3895d98e34f566146a060b49fc15e4c6870de91
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-01-23 14:50:37 +0300

    Update shortcut.ps1

--- shortcut.ps1
+++ shortcut.ps1
@@ -1,8 +1,4 @@
 $ComObj = New-Object -ComObject WScript.Shell
-$ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\quickassist.lnk")
-$ShortCut.TargetPath = "%windir%\system32\quickassist.exe"
-$ShortCut.FullName 
-$ShortCut.Save()
 $ShortCut = $ComObj.CreateShortcut("C:\Users\Public\Desktop\Outlook.lnk")
 $ShortCut.TargetPath = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
 $ShortCut.FullName 
